ab4d17ca351b8cf50d0f7bc41e81803228a3d126
Danira Cortez <cortezd334@gmail.com>
2020-05-16 20:54:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
 min = nil
 lowest_value = nil
@@ -13,4 +13,5 @@ lowest_value = nil
     end
   end
   min
+  binging pry
 end
