c21ab46241b6e6af33078836c8d7bc9bec4c4da8
Danira Cortez <cortezd334@gmail.com>
2020-05-16 21:22:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-min = nil
+#min = nil
 lowest_value = nil
 
   name_hash.each do |key, value|
 
     if lowest_value == nil || value < lowest_value 
     lowest_value = value
-    min = key
+    #min = key
     end
   end
-  min
+  #min
+  key
 end
